298f27440e4d00fb208e61223451e93d5c61cfcd
vladsft <vladixsafta@yahoo.com>
2025-02-11 20:40:35 +0000

    done reversed order lines

--- reversed-order/src/Main.java
+++ reversed-order/src/Main.java
@@ -1,15 +1,20 @@
-package reversed_order.src;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
+        Deque<String> myDeque = new ArrayDeque<>();
         while (line != null) {
-
+            myDeque.push(line);
+            line = br.readLine();
+        }
+        while (!myDeque.isEmpty()) {
+            System.out.println(myDeque.pop());
         }
     }
 }
