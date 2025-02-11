48c3bd9cde15bc1d14c9dc2a776a8bb7c312ce42
vladsft <vladixsafta@yahoo.com>
2025-02-11 20:03:55 +0000

    let's see if my git config is connected to github this time

---
+++ reversed_order/src/Main.java
@@ -0,0 +1,15 @@
+package reversed_order.src;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        while (line != null) {
+
+        }
+    }
+}
